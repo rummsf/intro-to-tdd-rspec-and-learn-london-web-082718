e6d19e33e4ae3a3d9c5029c8c30af066cb0250ab
Irum F <irummf@gmail.com>
2018-08-23 13:12:52 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,8 +1,3 @@
-def current_age_for_birth_year (year)
-	#your code here
+def current_age_for_birth_year(birth_year)
+  2018 - birth_year
 end
-
-#	birth_year = (1984)
-#current_age_for_birth_year (2018 - birth_year)
-#age_of_person = current_age_for_birth_year(1984)
-#end
